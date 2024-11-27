d477a324890a1795f1c91d05355ab4250559f5cd
FrimaNru <100356059+FrimaNru@users.noreply.github.com>
2024-11-27 18:50:54 +0300

    Update pz3.py

--- pz3.py
+++ pz3.py
@@ -1,66 +1,78 @@
-def levenstein(str_1, str_2):
-    n, m = len(str_1), len(str_2)
-    if n > m:
-        str_1, str_2 = str_2, str_1
-        n, m = m, n
+from Levenshtein import distance as levenshtein_distance
+from Levenshtein import ratio as levenshtein_ratio
+from fuzzywuzzy import fuzz
+import time
 
-    current_row = range(n + 1)
-    for i in range(1, m + 1):
-        previous_row, current_row = current_row, [i] + [0] * n
-        for j in range(1, n + 1):
-            add, delete, change = previous_row[j] + 1, current_row[j - 1] + 1, previous_row[j - 1]
-            if str_1[j - 1] != str_2[i - 1]:
-                change += 1
-            current_row[j] = min(add, delete, change)
+# Примеры строк для тестирования
+test_cases = [
+    ("машинное обучение", "обучение машинное"),
+    ("привет мир", "мир привет"),
+    ("Python programming", "Python programming language"),
+    ("искусственный интеллект", "искуственный интелект"),  # Опечатка
+    ("data science", "data scientist"),
+    ("hello world", "world hello"),
+    ("случайное совпадение", "случайное"),
+    ("12345", "123456"),
+    ("example string", "sample string"),
+    ("математика", "математический анализ"),
+]
 
-    return current_row[n]
-    
-str1 = "Привет мир"
-str2 = "Привет мир"
+# Функции для сравнения строк
+def compare_levenshtein(str1, str2):
+    start = time.time()
+    dist = levenshtein_distance(str1, str2)
+    ratio = levenshtein_ratio(str1, str2)
+    end = time.time()
+    return dist, ratio, end - start
 
-# 1 задание Звир/Северова
-print (f'0бычное сравнение\nПервое слово: {str1} \n Topое cлово: {str2}') 
-print(levenstein(str1, str2))
-# 2 задание Звир/Северова
-str1 = 'Привет мир'
-str2 = 'Привет мир!'
-print('\пчастичное сравнение\nПервое слово: (str1) \nВторое слово: (str2)') 
-print (levenstein(str1, str2))
+def compare_fuzzywuzzy(str1, str2):
+    start = time.time()
+    ratio = fuzz.ratio(str1, str2)
+    partial_ratio = fuzz.partial_ratio(str1, str2)
+    token_sort = fuzz.token_sort_ratio(str1, str2)
+    token_set = fuzz.token_set_ratio(str1, str2)
+    end = time.time()
+    return ratio, partial_ratio, token_sort, token_set, end - start
 
+def levenshtein_manual(s1, s2):
+    start = time.time()
+    len_s1, len_s2 = len(s1), len(s2)
+    dp = [[0] * (len_s2 + 1) for _ in range(len_s1 + 1)]
 
-# 3 задание Звир/Северова
-str1 = "Привет наш мир"
-str2 = "наш Привет мир"
-print(f" \nСравнение по токену первое слово: {str1} \nВторое слово: {str2}")
-print(levenstein(str1, str2))
+    for i in range(len_s1 + 1):
+        for j in range(len_s2 + 1):
+            if i == 0:
+                dp[i][j] = j
+            elif j == 0:
+                dp[i][j] = i
+            elif s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+    dist = dp[len_s1][len_s2]
+    ratio = 1 - dist / max(len_s1, len_s2)
+    end = time.time()
+    return dist, ratio, end - start
 
-# 4 задание Звир/Северова
-str1 = "Привет мир"
-str2 = "!ПриВЕт, наш мир!"
-print (f' \nПродвинутое обычно сравнение\n Первое слово: {str1} \nВторое слово {str2}')
-print (levenstein(str1, str2))
+# Автоматическое тестирование
+results = []
+for case in test_cases:
+    str1, str2 = case
+    lev_result = compare_levenshtein(str1, str2)
+    fuzzy_result = compare_fuzzywuzzy(str1, str2)
+    manual_result = levenshtein_manual(str1, str2)
 
+    results.append({
+        "Strings": (str1, str2),
+        "Levenshtein": lev_result,
+        "FuzzyWuzzy": fuzzy_result,
+        "Manual Levenshtein": manual_result
+    })
 
-#5 задание Звир/Северова
-strl = "Саратов"
-city = ["Москва", "Санкт-Петербург", "Саратов", "Краснодар", "Воронеж", "Омск", "Екатеринбург", "Орск", "Красногорск", "Красноярск", "Самaра"]
-
-ans = sorted([[levenstein(str1, i), i] for i in city], key=lambda x: x[0])
-print(f' \nСписок\nПервое слово: {str1} \n{ans}')
-
-
-from fuzzywuzzy import fuzz, process
-a = fuzz.ratio("Привет мир", "Привет мир")
-print(a)
-a = fuzz.partial_ratio("Привет мир", "Привет мир!")
-print(a)
-a = fuzz.partial_ratio("Привет мир", 'Люблю колбасу', "Привет мир") 
-print(a)
-a = fuzz.token_sort_ratio( 'Привет наш мир', "мир наш Привет")
-print(a)
-a = fuzz.wratio('Привет наш мир', "ПриВЕТ наш мир!")
-print(a)
-city = ["Москва", "Санкт-Петербург", "Саратов", "Краснодар", "Воронеж", "Омск", "Екатеринбург", "Орск", "Красногорск", "Красноярск", "Самара"]
-a = process.extract("Саратов", city)
-# Параметр limit по умолчанию имеет значение 5
-print(a)
+# Вывод результатов
+for res in results:
+    print(f"Сравнение строк: {res['Strings']}")
+    print(f"  Levenshtein (dist, ratio, time): {res['Levenshtein']}")
+    print(f"  FuzzyWuzzy (ratio, partial, token_sort, token_set, time): {res['FuzzyWuzzy']}")
+    print(f"  Manual Levenshtein (dist, ratio, time): {res['Manual Levenshtein']}")
+    print("-" * 80)
