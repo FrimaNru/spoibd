def2c114340c07ff33102698afa5dc595b33fd64
FrimaNru <100356059+FrimaNru@users.noreply.github.com>
2024-12-24 21:40:26 +0300

    create lr2 проект

---
+++ lr2/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://047084354_zvir:Arti2005@mysql.j1007852.myjino.ru:3306/j1007852_237_zvir"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "e2c4b9274f1e7b6a8b9f0d4f7c3a9d12"
